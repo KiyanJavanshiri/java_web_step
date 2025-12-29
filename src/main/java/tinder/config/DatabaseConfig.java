02b36ae5e6c9b790eedb6a13877874e4d5756a3f
kiyan javanshiri <kiyanjavanshiri@gmail.com>
2025-12-29 11:50:31 +0200

    feat: add db config

---
+++ src/main/java/tinder/config/DatabaseConfig.java
@@ -0,0 +1,15 @@
+package tinder.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConfig {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/tinder";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "France_trump_JV-123";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+    }
+}
