2080613f4eff138297d6aa4eebe02f4952365941
kiyan javanshiri <kiyanjavanshiri@gmail.com>
2026-01-01 12:23:17 +0200

    fix: minor change on db config

--- src/main/java/tinder/config/DatabaseConfig.java
+++ src/main/java/tinder/config/DatabaseConfig.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConfig {
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/tinder";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/YOUR_DB_NAME";
     private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "France_trump_JV-123";
+    private static final String DB_PASSWORD = "YOUR_DB_PASSWORD";
 
     public static Connection getConnection() {
         try {
